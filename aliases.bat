02b5e2ab43e8b8484789a87c5efdd9457b9a1aab
Kyle Towner <33582642+ktowner15@users.noreply.github.com>
2022-05-10 12:24:51 -0500

    Add files via upload
    Added aliases.bat, for automatic import at shell startup.

---
+++ aliases.bat
@@ -0,0 +1,14 @@
+@REM cmd aliases.  Must be run every time a cmd session starts, otherwise they won't work.
+
+@REM PYTHON 3 VIRTUAL ENVIRONMENT create/list/delete/activate aliases
+@REM USAGE:
+@REM    pyVirt-n (new environment name) === creates new environment named appropriately
+@REM    pyVirt-l === lists all virtual environments
+@REM    pyVirt-r (environment to be removed) === removes specified virtual environment
+@REM    pyVirt (environment name) === activate specified environment
+@REM    All instances can be exited with 'deactivate'.
+
+doskey pyVirt-n=python -m venv C:\Applications\Python_virtual_envs\$*
+doskey pyVirt-l=dir C:\Applications\Python_virtual_envs
+doskey pyVirt-r=powershell rmdir -r C:\Applications\Python_virtual_envs\$*
+doskey pyVirt=C:\Applications\Python_virtual_envs\$*\Scripts\activate.bat
